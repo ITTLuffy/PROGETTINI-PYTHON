9dcdd5573ca8d223c197ffbc82a4271e582a5874
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-09-22 09:07:54 +0200

    Init repo

---
+++ test.py
@@ -0,0 +1,21 @@
+counter = 0
+counter2 = 1
+print(type(counter))
+
+# while counter > 0:
+#    print(counter - 1)
+#    counter -= 1
+
+# IT ASSISTANT 
+
+# for i in range(5):
+#    print(i)
+
+while counter <= 50 and counter % 2 != 0:
+    print(counter)
+    counter += 1 
+    counter2 += 1 
+
+
+
+# // operatore per la divisione intera
